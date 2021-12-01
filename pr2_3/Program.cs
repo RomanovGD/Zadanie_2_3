462a52a35f556db5d7df77e911f8927f79d78954
RomanovGD <95343882+RomanovGD@users.noreply.github.com>
2021-12-01 17:27:08 +0300

    Add files via upload

---
+++ pr2_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace pr2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100, p, t;
+            Console.WriteLine("Задание III.\n");
+            Console.WriteLine("while");
+            while (n < 1000)
+            {
+                p = Convert.ToInt32(n / 100);
+                t = n % 10;
+                if (p==t)
+                    Console.WriteLine($"\t{n}");
+                n ++;
+            }
+            n = 100;
+            Console.WriteLine();
+            Console.WriteLine("do...while");
+            do
+            {
+                p = Convert.ToInt32(n / 100);
+                t = n % 10;
+                if (p == t)
+                    Console.WriteLine($"\t{n}");
+                n ++;
+            } while (n < 1000);
+            Console.WriteLine();
+            Console.WriteLine("for");
+            for (n = 100; n < 1000; n ++)
+            {
+                p = Convert.ToInt32(n / 100);
+                t = n % 10;
+                if (p == t)
+                    Console.WriteLine($"\t{n}");
+            }
+
+        }
+    }
+}
